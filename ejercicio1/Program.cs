7d65ff5ea7ebb8ccf647f8efcd3882be95e1c2b6
fedecachin <78211537+fedecachin@users.noreply.github.com>
2022-08-23 16:03:38 -0300

    Add files via upload

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerosIngresado;
+            int maximo = 0;
+            int minimo = 0;
+            float promedio = 0;
+            float promedioFinal;
+            bool esNumerico;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("ingrese un numero");
+                esNumerico = int.TryParse(Console.ReadLine(), out numerosIngresado);
+                promedio += numerosIngresado;
+
+                if (numerosIngresado > maximo)
+                {
+                    maximo = numerosIngresado;
+                }
+                else
+                {
+                    if (numerosIngresado < minimo)
+                    {
+                        minimo = numerosIngresado;
+                    }
+                }
+            }
+            
+            if (esNumerico)
+            {
+                promedioFinal = promedio / 5;
+
+                Console.WriteLine($"el maximo es {maximo}, el minimo es {minimo} y el promedio es {promedioFinal}");
+            } else
+            {
+                Console.WriteLine($"alguno de los datos ingresados no es numerico");
+            }
+            
+        }
+    }
+}
