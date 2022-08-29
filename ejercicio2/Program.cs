5952ed02203f7c0538817cf70c0e121c26862547
fedecachin <78211537+fedecachin@users.noreply.github.com>
2022-08-29 18:25:01 -0300

    Add files via upload

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar un número y mostrar el cuadrado y
+            //el cubo del mismo. Se debe validar que el número sea mayor
+            //que cero, caso contrario, mostrar el
+            //mensaje: "ERROR. ¡Reingresar número!".
+
+            double numeroIngresado;
+            bool esNumerico;
+            int bandera = 1;
+            double cuadrado;
+            double cubo;
+
+            Console.WriteLine("ingrese un numero");
+            do
+            {
+                if (bandera == 0)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número!");
+                }
+                esNumerico = double.TryParse(Console.ReadLine(), out numeroIngresado);
+
+                bandera = 0;
+
+            } while (esNumerico);
+
+            cuadrado = Math.Pow(numeroIngresado, 2);
+            cubo = Math.Pow(numeroIngresado, 3);
+
+            Console.WriteLine($"numero al cuadrado {cuadrado}. numero al cubo {cubo}");
+        }
+    }
+}
